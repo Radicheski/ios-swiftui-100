73ff72aed0cb359997365fed00fc482602981527
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-09-29 16:59:31 -0300

    Finished Project 2, part two

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Erik Radicheski da Silva on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
