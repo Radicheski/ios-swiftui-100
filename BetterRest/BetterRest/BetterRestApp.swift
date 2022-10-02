984c9004a517812780ca2e024e0a2d183121b8f9
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-10-02 17:04:40 -0300

    Finished Project 4, part two

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Erik Radicheski da Silva on 02/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
