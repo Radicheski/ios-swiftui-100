1db368812dc9d9e32419f2a86fc99f642a6b0192
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-10-03 13:22:57 -0300

    Finished Project 5, part two

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Erik Radicheski da Silva on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
