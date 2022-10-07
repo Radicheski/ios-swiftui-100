bdeb969825b8c68ba3fafc7c5633262a8f166b0a
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-10-07 17:34:44 -0300

    Finished Project 8, part two

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Erik Radicheski da Silva on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
