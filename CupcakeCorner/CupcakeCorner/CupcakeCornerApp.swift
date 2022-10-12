908b011258476e0417258b227f833f00e62ba69c
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-10-12 15:57:53 -0300

    Finished Project 10, part two

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Erik Radicheski da Silva on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,54 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Erik Radicheski da Silva on 12/10/22.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAddress: Bool {
+        guard name.isEmpty == false else { return false }
+        guard streetAddress.isEmpty == false else { return false }
+        guard city.isEmpty == false else { return false }
+        guard zip.isEmpty == false else { return false }
+        return true
+    }
+    
+    var cost: Double {
+        var cost = Double(quantity) * 2
+        cost += Double(type) / 2
+        
+        if extraFrosting {
+            cost += Double(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+        
+        return cost
+    }
+}
