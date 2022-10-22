799ac8ef243c5ebb30297a98a86b2c336fe453fc
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-10-22 15:04:25 -0300

    Finished Project 16, part four

---
+++ HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Erik Radicheski da Silva on 22/10/22.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        people = []
+    }
+}
