c7d8fc90708fc13d87aba977ce6e5f58eba7a598
Erik Radicheski da Silva <erik.radicheski@gmail.com>
2022-10-24 15:30:22 -0300

    Finished Project 17, part three

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Erik Radicheski da Silva on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
